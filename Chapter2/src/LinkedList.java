afb709726ef0cce49236c379e1e025f8745be4ab
Leo Meira Vital <leo.vital@nubank.com.br>
2020-04-29 21:26:16 -0300

    Adding solution to 2.4

--- Chapter2/src/LinkedList.java
+++ Chapter2/src/LinkedList.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Optional;
 
 public class LinkedList {
 
@@ -31,25 +32,93 @@ public class LinkedList {
         }
     }
 
-    public static void main(String[] args) {
-        Node n = new Node(5);
-        n.appendToTail(5);
-        n.appendToTail(3);
-        n.appendToTail(5);
-        n.appendToTail(4);
-        n.appendToTail(5);
-        n.appendToTail(5);
-        n.appendToTail(4);
-        n.appendToTail(4);
-        LinkedList.removeDuplicates(n);
-
-        Node tmp = n;
-
-        while (tmp != null) {
-            System.out.println("---");
-            System.out.println(tmp.data);
-            System.out.println("---");
-            tmp = tmp.next;
+    // I actually had a stupid solution originally, iterating through all nodes starting from n.
+    // This was the solution suggested by the book.
+    static boolean removeGivenNode(Node n) {
+        if (n == null || n.next == null) {
+            return false;
         }
+
+        n.data = n.next.data;
+        n.next = n.next.next;
+
+        return true;
+    }
+
+    public static int zeroIfNull(Integer num) {
+        return Optional.ofNullable(num).orElse(0);
+    }
+
+    static Node addIntegers(Node n1, Node n2) {
+        if (n1 == null && n2 == null) {
+            return null;
+        }
+
+        Node head = new Node(0);
+        int headData = 0;
+        int remainder;
+
+        if (n1 != null) {
+            headData += n1.data;
+            n1 = n1.next;
+        }
+
+        if (n2 != null) {
+            headData += n2.data;
+            n2 = n2.next;
+        }
+
+        head.data = headData % 10;
+        remainder = headData / 10;
+
+
+        while (n1 != null && n2 != null) {
+            int resultData = n1.data + n2.data + remainder;
+            remainder = resultData / 10;
+            head.appendToTail(resultData % 10);
+            n1 = n1.next;
+            n2 = n2.next;
+        }
+
+        while (n1 != null) {
+            int resultData = n1.data + remainder;
+            remainder = resultData / 10;
+            head.appendToTail(resultData % 10);
+            n1 = n1.next;
+        }
+
+        while (n2 != null) {
+            int resultData = n2.data + remainder;
+            remainder = resultData / 10;
+            head.appendToTail(resultData % 10);
+            n2 = n2.next;
+        }
+
+        return head;
+    }
+
+    public static void printNode(Node n) {
+        while (n != null) {
+            System.out.print(n.data);
+            System.out.print(" -> ");
+            n = n.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        Node n1 = new Node(4);
+        n1.appendToTail(4);
+
+        Node n2 = new Node(9);
+        n2.appendToTail(3);
+        n2.appendToTail(2);
+//        LinkedList.removeDuplicates(n);
+//        LinkedList.removeGivenNode(nodeFour);
+
+        Node result = addIntegers(n1, n2);
+
+        printNode(result);
+
+        System.out.print("null");
     }
 }
--- Chapter2/src/Node.java
+++ Chapter2/src/Node.java
@@ -1,13 +1,18 @@
 public class Node {
     Node next = null;
     int data;
+
+    public Node() {}
+
     public Node(int d) { data = d; }
-    void appendToTail(int d) {
+
+    Node appendToTail(int d) {
         Node end = new Node(d);
         Node n = this;
         while (n.next != null) {
             n = n.next;
         }
         n.next = end;
+        return end;
     }
 }
